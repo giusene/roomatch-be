5e9e6efd3d95664a8b82de456b0fccc3770def79
g.sen <giu.sene@gmail.com>
2022-02-04 13:19:58 +0100

    first commit

---
+++ config.js
@@ -0,0 +1,10 @@
+
+
+const config = {
+    uriKey:'Giocollov1'
+}
+
+module.exports = config
+
+
+// Me2hMydCDfYD2c7
--- libs/mongoFunctions.js
+++ libs/mongoFunctions.js
@@ -2,6 +2,7 @@ require('dotenv').config({path: '.env'});
 const { MongoClient, ObjectId, ObjectID } = require("mongodb");
 const uri = process.env.NODE_HEROKU_URI_KEY;
 
+
 const dbURI = `mongodb+srv://feisbrut:${uri}@feisbrut-db.unhhv.mongodb.net/posts?retryWrites=true&w=majority`;
 const mongoClient = new MongoClient(dbURI);
 const dbURI2 = `mongodb+srv://feisbrut:${uri}@feisbrut-db.unhhv.mongodb.net/users?retryWrites=true&w=majority`;
@@ -39,6 +40,7 @@ async function insertFunction(collection,obj,filter){
 async function deleteFunction(collection,filter){
     await feisbrutDB.collection(collection).deleteOne(filter);
 }
+
 
 /* --------------------------------------------------------------CONNECTIONS------------------------------------------------------------------------ */
 async function run1() {
